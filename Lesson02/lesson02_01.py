633b80a5cf7c72e5ace17f0b97ba2592de4836b8
orlova-lb <66432081+orlova-lb@users.noreply.github.com>
2020-06-06 23:24:29 +0300

    first commit

---
+++ Lesson02/lesson02_01.py
@@ -0,0 +1,3 @@
+
+print('Escape sequences\n\\a\tBell (alert)\n\\b\tBackspace \n\\n\tNew line\n\\t\tHorizontal tab\n\
+\\\\\tBackslash \\ \n\\\"\tDouble quotation mark "\n\\\'\tSingle quotation mark \'')
