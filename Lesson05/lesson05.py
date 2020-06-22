13116952cb5113b7d54bd8c96a1564d3ff034481
orlova-lb <66432081+orlova-lb@users.noreply.github.com>
2020-06-22 22:32:12 +0300

    lesson 05 vs 2

--- Lesson05/lesson05.py
+++ Lesson05/lesson05.py
@@ -14,6 +14,9 @@ print()
 
 
 # заполненный треугольник
+
+height = int(input('Please enter the height of figure:\t'))
+width = height * 2 - 1
 print(' "B"')
 for i in range(height):
     for j in range(width):
@@ -27,41 +30,45 @@ print()
 
 
 # ромб 1
+height = int(input('Please enter the height of figure:\t'))
+width = height * 2 -1
 print(' "C"')
-for i in range(height):
+for i in range(height // 2):
     for j in range(width):
-        if height - 1 - i <= j <= height - 1 + i:
+        if height // 2 - 1 - i <= j <= height // 2 - 1 + i:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
-for i in range(height):
+for i in range(height // 2):
     if i == 0:
         continue
     for j in range(width):
-        if i == j or i == width - j - 1:
+        if i == j or i == width // 2 - j -1:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
 print()
 
-# ромб 2
+height = int(input('Please enter the height of figure:\t'))
+width = height * 2 - 1
 print(' "D"')
-for i in range(height):
+for i in range(height // 2):
     for j in range(width):
-        if height - 1 - i <= j <= height - 1 + i:
+        if height // 2 - 1 - i <= j <= height // 2 - 1 + i:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
-for i in range(height):
+for i in range(height // 2):
     if i == 0:
         continue
     for j in range(width):
-        if i == j or i == width - j - 1 or j == width // 2:
+        if i == j or i == width // 2 - j -1 or j == height // 2 - 1:
             print('* ', end='')
         else:
             print('  ', end='')
     print()
 print()
+
