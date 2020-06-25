e94f01e4383d3520126008cb49b9408938fecf02
orlova-lb <66432081+orlova-lb@users.noreply.github.com>
2020-06-25 17:12:40 +0300

    homework_14 vs 1

--- Lesson06/homework_14.py
+++ Lesson06/homework_14.py
@@ -1,1 +1,1 @@
-s = print(len(set(input('Введите первый список : ').split())& set(input('Введите второй список : ').split())))
+s = print(len(set(input('Введите первый список : ').split())| set(input('Введите второй список : ').split())))
