193ccd6d9131310a9aab76c55142a5f010016f3a
orlova-lb <66432081+orlova-lb@users.noreply.github.com>
2020-06-30 21:17:51 +0300

    homework_18 vs1

---
+++ Lesson07/Homework_18.py
@@ -0,0 +1,17 @@
+"""Написать функцию season, принимающую 1 аргумент — номер месяца (от 1 до 12),
+ и возвращающую время года, которому этот месяц принадлежит (зима, весна, лето или осень)."""
+
+
+def season(month):
+    if month in (12, 1, 2):
+        return "Зима"
+    elif month in (3, 4, 5):
+        return "Весна"
+    elif month in (6, 7, 8):
+        return "Лето"
+    elif month in (9, 10, 11):
+        return "Осень"
+
+
+month = int(input('Введите номер месяца : '))
+print(season(month))
