2a64a626a75c28e6826bb15df2e15d618d001fd1
orlova-lb <66432081+orlova-lb@users.noreply.github.com>
2020-07-06 15:55:12 +0300

    vs 2

--- Lesson07/Homework_16.py
+++ Lesson07/Homework_16.py
@@ -1,3 +1,8 @@
+"""
+Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True,
+если год високосный, и False иначе.
+"""
+
 def is_year_leap(year):
     if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
         return 'True'
